fba001512b0ce132d9b5bd2659ce41cfec567fe7
Nijamudeen <nijam96@yahoo.co.in>
2016-12-30 19:29:38 +0000

    Login CSS Done

--- login/urls.py
+++ login/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import  url
 from . import views
 
-urlpatterns = [ url(r'^$',views.index, name = 'index')]
+urlpatterns = [ url(r'^$',views.index, name = 'index'),
+				url(r'^auth/$',views.auth, name = 'auth')]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
-from django.shortcuts import render_to_response
+from django.shortcuts import render,render_to_response
+from django.http import HttpResponse
+import json
+from django.contrib.auth import authenticate
+
 
 def index(request):
 	return render(request,"home.html",{})
+
+def auth(request):
+	user = request.POST['name']
+	password = request.POST['pass']
+	user = authenticate(username=user, password=password)
+	if user is not None:
+		print "Y"
+		# A backend authenticated the credentials
+	else:
+		print "N"
+		# No backend authenticated the credentials
+	print user,password
+	return HttpResponse(0)
