c6d7dd215c4f3854af5181ff3784b5714de8f1fb
Nijamudeen <nijam96@yahoo.co.in>
2017-01-25 11:36:04 +0000

    production

--- bookingsystem_admin/production.py
+++ bookingsystem_admin/production.py
@@ -1,6 +1,21 @@
-from bookingsystem_admin.settings import *
+#Production Settings
+import os
 
-DEBUG = True
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DBBACKUP_STORAGE = 'storage.AzureStorage'
+
+with open('/etc/production.txt') as f:
+    lines = f.readlines()
+    SECRET_KEY = lines[0].strip() 
+    AZURE_ACCOUNT_NAME = lines[1].strip() 
+    AZURE_ACCOUNT_KEY = lines[2].strip() 
+    AZURE_CONTAINER = lines[3].strip() 
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
@@ -17,3 +32,82 @@ DATABASES = {
         'HOST': 'mysql2704.cloudapp.net',
     }
 }
+
+AUTH_USER_MODEL = 'login.CustomUser'
+
+DATABASE_ROUTERS = ['bksys.router.DBRouter']
+
+INSTALLED_APPS = [
+    'login',
+    'bksys',
+    'storages',
+    'dbbackup',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'bookingsystem_admin.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'bookingsystem_admin.wsgi.application'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-uk'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+STATIC_URL = '/static/'
